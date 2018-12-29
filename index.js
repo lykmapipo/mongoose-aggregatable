75c36017593023024ffdbebc3c3d500c8b309779
lykmapipo <lallyelias87@gmail.com>
2018-12-29 19:12:16 +0300

    Collect aggragatable paths

--- index.js
+++ index.js
@@ -2,3 +2,86 @@
 
 
 /* dependencies */
+const _ = require('lodash');
+const { eachPath } = require('@lykmapipo/mongoose-common');
+
+
+/**
+ * @function collectAggregatables
+ * @name collectAggregatables
+ * @description collect schema aggregatable fields
+ * @param {String} pathName path name
+ * @param {SchemaType} schemaType SchemaType of a path
+ * @return {Object} hash of all schema aggregatable paths
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @private
+ * const aggregatables = collectAggregatables(schema);
+ */
+function collectAggregatables(schema) {
+  // aggregatable map
+  const aggregatables = {};
+
+  // collect aggregatable schema paths
+  eachPath(schema, function collectAggregatablePath(pathName, schemaType) {
+
+    // obtain path schema type options
+    const schemaTypeOptions = _.merge({},
+      _.get(schemaType, 'caster.options'),
+      _.get(schemaType, 'options')
+    );
+
+    // check if path is aggregatable
+    const isAggregatable =
+      (schemaTypeOptions && schemaTypeOptions.aggregatable);
+
+    // if aggregatable collect
+    if (isAggregatable) {
+      // console.log(pathName, schemaType);
+      // obtain aggregatable options
+      const aggregatableOptions =
+        _.pick(schemaTypeOptions, 'ref', 'aggregatable');
+      /*TODO {
+       from: <collection to join>,model(options.ref).collection.name || options.from
+       localField: <field from the input documents>,path
+       foreignField: <field from the documents of the "from" collection>,_id
+       as: <output array field>, path or options.path
+     }*/
+      // collect aggregatable schema path
+      aggregatables[pathName] = _.merge({}, aggregatableOptions);
+    }
+  });
+
+  // return collect aggregatable schema paths
+  return aggregatables;
+}
+
+
+/**
+ * @function aggregatable
+ * @name aggregatable
+ * @description mongoose plugin to add aggregations behaviour. 
+ * @param {Schema} schema valid mongoose schema
+ * @param {Object} [optns] plugin options
+ * @author lally elias <lallyelias87@mail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @public
+ * @example
+ * const aggregatable = require('@lykmapipo/mongoose-aggregatable');
+ * const UserSchema = 
+ * 	new Schema({ parent: { type: ObjectId, ref:'User', aggregatable:true } });
+ * UserSchema.plugin(aggregatable);
+ */
+function aggregatable(schema /*, options*/ ) {
+  // collect aggregatables schema paths
+  const aggregatabless = collectAggregatables(schema);
+  schema.statics.AGGREGATABLE_FIELDS = aggregatabless;
+}
+
+
+/* export aggregatable plugin */
+module.exports = exports = aggregatable;
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,33 +2,25 @@
 
 
 /* dependencies */
-const _ = require('lodash');
-const faker = require('@benmaruchu/faker');
-// const { include } = require('@lykmapipo/include');
-// const { expect } = require('chai');
+const { include } = require('@lykmapipo/include');
+const { expect } = require('chai');
 const { model, Schema, ObjectId } = require('@lykmapipo/mongoose-common');
 const { clear } = require('@lykmapipo/mongoose-test-helpers');
+const taggable = include(__dirname, '..');
 
 
 /* prepare schemas */
-const FriendSchema = new Schema({
-  type: { type: String, fake: { generator: 'name', type: 'findName' } },
-  person: { type: ObjectId, ref: 'Person', aggregatable: true }
-});
-
 const PersonSchema = new Schema({
   name: { type: String, fake: { generator: 'name', type: 'findName' } },
   father: { type: ObjectId, ref: 'Person', aggregatable: true },
   mother: { type: ObjectId, ref: 'Person', aggregatable: true },
   sister: { type: ObjectId, ref: 'Person', aggregatable: true },
   relatives: { type: [ObjectId], ref: 'Person', aggregatable: true },
-  referees: [{ type: ObjectId, ref: 'Person', aggregatable: true }],
-  friends: { type: [FriendSchema] },
-  neighbours: [FriendSchema]
+  friends: [{ type: ObjectId, ref: 'Person', aggregatable: true }]
 });
 
 PersonSchema.plugin(require('@lykmapipo/mongoose-faker'));
-// PersonSchema.plugin(include(__dirname, '..'));
+PersonSchema.plugin(taggable);
 const Person = model('Person', PersonSchema);
 
 
@@ -38,9 +30,7 @@ describe('aggregatable', function () {
   let mother = Person.fake();
   let sister = Person.fake();
   let relatives = [Person.fake(), Person.fake()];
-  let referees = [Person.fake(), Person.fake()];
   let friends = [Person.fake(), Person.fake()];
-  let neighbours = [Person.fake(), Person.fake()];
 
   before(done => clear(done));
 
@@ -73,34 +63,25 @@ describe('aggregatable', function () {
   });
 
   before((done) => {
-    Person.insertMany(referees, (error, created) => {
-      referees = created;
-      done(error, created);
-    });
-  });
-
-  before((done) => {
     Person.insertMany(friends, (error, created) => {
-      friends = _.map(created, (friend) => {
-        return {
-          type: faker.hacker.ingverb(),
-          person: friend
-        };
-      });
+      friends = created;
       done(error, created);
     });
   });
 
-  before((done) => {
-    Person.insertMany(neighbours, (error, created) => {
-      neighbours = _.map(created, (neighbour) => {
-        return {
-          type: faker.hacker.ingverb(),
-          person: neighbour
-        };
-      });
-      done(error, created);
-    });
+  it('should collect aggregatable paths', () => {
+    console.log(Person.AGGREGATABLE_FIELDS);
+    expect(Person.AGGREGATABLE_FIELDS).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.father).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.mother).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.sister).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.relatives).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.friends).to.exist;
+  });
+
+  it('should not collect non aggregatable paths', () => {
+    expect(Person.AGGREGATABLE_FIELDS).to.exist;
+    expect(Person.AGGREGATABLE_FIELDS.name).to.not.exist;
   });
 
   after(done => clear(done));
